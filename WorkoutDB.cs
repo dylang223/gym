6ff9310079cdd6352ee644621bcefda5839f7dc4
dylang223 <glynndylan36@gmail.com>
2025-05-15 23:32:17 +0100

    changed workouts to be a local litedb

---
+++ WorkoutDB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiteDB;
+
+namespace gym
+{
+    public class WorkoutDB
+    {
+        private readonly string _dbPath = "workout.db";
+        
+        public void InsertWorkout(Workout workout)
+        {
+            using var db = new LiteDatabase(_dbPath);
+            var col = db.GetCollection<Workout>("workouts");
+            col.Insert(workout);
+        }
+
+        public List<Workout> GetAllWorkouts()
+        {
+            using var db = new LiteDatabase(_dbPath);
+            var col = db.GetCollection<Workout>("workouts");
+            return col.FindAll().ToList();
+        }
+    }
+}
